a377c1daefdcdd785d619f4c8042d7a8759ee38d
Tedd Chee <jchee86@gmail.com>
2021-06-01 16:55:37 -0500

    add category sidebar

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -4,19 +4,27 @@ import Link from "next/link"
 import matter from "gray-matter"
 import Layout from "@/components/Layout"
 import Post from "@/components/Post"
+import CategoryList from "@/components/CategoryList"
 import { getPosts } from "@/lib//posts"
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>
-        Posts in {categoryName}
-      </h1>
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            Posts in {categoryName}
+          </h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
       <Link href='/blog'>
         <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
@@ -56,6 +64,9 @@ export async function getStaticProps({ params: { category_name } }) {
 
   const posts = getPosts()
 
+  const categories = posts.map((post) => post.frontmatter.category)
+  const uniqueCategories = [...new Set(categories)]
+
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category === category_name
   )
@@ -64,6 +75,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   }
 }
